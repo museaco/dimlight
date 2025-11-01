c3cdc4a4f9248392679714d3626377acdc058fde
museaco <hookfish007@gmail.com>
2025-11-01 22:47:59 +0800

    add

--- js/index.js
+++ js/index.js
@@ -4,24 +4,36 @@ gsap.registerPlugin(ScrollTrigger);
 $(window).on('load', function() {
   $('#page-loading').remove();
 
-  gsap.from('img[data-img]', {
+  gsap.from('img[data-img-y-show]', {
     y: 20,
     autoAlpha: 0,
     stagger: 0.25,
   });
 
-  const dy_tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '#dy-section',
-      scrub: true,
-      start: 'top bottom',
-      end: 'bottom bottom',
-      // markers: true,
-      toggleActions: 'play none none reverse',
-    },
+  // const dy_tl = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: '#dy-section',
+  //     scrub: true,
+  //     start: 'top bottom',
+  //     end: 'bottom bottom',
+  //     // markers: true,
+  //     toggleActions: 'play none none reverse',
+  //   },
+  // });
+  //
+  // dy_tl.from('#dy-section', { opacity: 0, y: '1.8rem', duration: 1 });
+
+  gsap.utils.toArray(['.from-y-show']).forEach(section => {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        scrub: true,
+        start: 'top bottom',
+        end: '50% bottom',
+      },
+    }).from(section, { opacity: 0, y: '2rem', duration: 1 });
   });
 
-  dy_tl.from('#dy-section', { opacity: 0, y: '1.8rem', duration: 1 });
 
   const fl_tl = gsap.timeline({
     scrollTrigger: {
